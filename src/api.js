864ba32fd57d3c22e4ff6324aca6bb0e802d5829
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2022-07-12 22:48:28 -0300

    Add dotenv vars

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import axios from "axios";
 
 const callApi = (path, token) => {
   return axios.create({
-    baseURL: `https://debugapp-keeper-api.herokuapp.com/${path}`,
+    baseURL: `${process.env.URL_API}/${path}`,
     headers: {
       "content-type": "application/json",
       "x-access-token": `${token}`,
