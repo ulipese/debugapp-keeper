df8f602abd9471a284bed5605bd377ec4f24fb07
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2022-07-21 14:45:02 -0300

    Test Netlify v1

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
-
+console.log(process.env);
+console.log(process.env.REACT_APP_API_URL);
 const callApi = (path, token) => {
   return axios.create({
     baseURL: `${API_URL}/${path}`,
